eb8657859d9b4d934383cd6e2de4cf5b9fca5c30
IFirstTeaLover <pasabozenko3@gmail.com>
2024-11-11 08:54:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-setWidth()
-},1000);
-
-function setWidth() {
-  document.documentElement.style.setProperty('--width', Math.round(globalThis.outerWidth/6.7));// if more links then change that
-  document.documentElement.style.setProperty('--upgradewidth', Math.round(globalThis.outerWidth));}// if more upgrades then change that
